e539cc4687d9aee07c73b227dc7fa3a1c1c4d5a6
hramos-git <ramoshuascar@gmail.com>
2026-03-06 12:58:56 -0400

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,8 @@
+//alert("Hola cómo estás?");
+/*
+console.log("Hola mundo");
+console.warn("Parece que hay una advertencia");
+console.error("Y que sepas que tienes un error");
+*/
+var edad = 42;
+console.log("El doble de tu edad es: " + (edad*2));
